6c6e4aac3b083363ab15203bcf099cd2b3ee9ecd
Max Burkhardt <maxb@openai.com>
2026-05-05 12:09:08 -0700

    Support Typescript language server

---
+++ .config/nvim/after/plugin/typescript.lua
@@ -0,0 +1,12 @@
+vim.lsp.config("ts_ls", {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+})
+
+vim.lsp.enable("ts_ls")
